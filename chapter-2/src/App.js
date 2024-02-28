cd75467e91fed8601ef5cfcaa28d0282c86de2a1
ChandraPrakash24 <2412prakas@gmail.com>
2024-02-28 18:25:18 +0530

    Learning reading dynamic url params

--- chapter-2/src/App.js
+++ chapter-2/src/App.js
@@ -6,6 +6,7 @@ import FooterComponent from "./Components/FooterComponent";
 import About from "./Components/About";
 import Error from "./Components/Error";
 import Contact from "./Components/Contact";
+import RestaurentMenu from "./Components/RestaurentMenu";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 const AppLayout = () => {
@@ -36,9 +37,17 @@ const appRouter = createBrowserRouter([
       {
         path: "/contact",
         element: <Contact />
+      },
+      {
+        path: "/restaurent/:id",
+        element: <RestaurentMenu />
       }
     ]
   }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(<RouterProvider router={appRouter} />);
+
+// https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.71700&lng=75.83370&restaurantId=67970&catalog_qa=undefined&submitAction=ENTER
+
+// https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.71700&lng=75.83370&restaurantId=84070&catalog_qa=undefined&submitAction=ENTER
--- chapter-2/src/Components/RestaurentMenu.js
+++ chapter-2/src/Components/RestaurentMenu.js
@@ -1,9 +1,53 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 const RestaurentMenu = () => {
-  return (
-    <div>
+    const param = useParams();
+    // const { id } = useParams(); // destructure on the fly
+    // console.log(param);
+    const { id } = param; // destructure of param
+
+    const [RestaurentMenuInfoData, setRestaurentMenuInfoData] = useState();
+    useEffect(()=>{
+        getRestaurentInfo();
+    },[]);
+    
+    async function getRestaurentInfo() {
+        const RestaurentInfoData = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.71700&lng=75.83370&restaurantId=84070&catalog_qa=undefined&submitAction=ENTER");
+    
+        const parsedRestaurentInfoData = await RestaurentInfoData.json();
+        // console.log(parsedRestaurentInfoData.data);
+        //data.cards[4].groupedCard.cardGroupMap.REGULAR.cards[1].card.card.itemCards[6].card.info.name
+        setRestaurentMenuInfoData(parsedRestaurentInfoData);
+    }
+    
+    // console.log(parsedRestaurentInfoData);
+    // console.log(RestaurentMenuInfoData);
+    // setTimeout(()=>{console.log(RestaurentMenuInfoData.data.cards[0].card.card.info.name);},10000)
+    //     OR
+    // useEffect(() => {
+    //     if (RestaurentMenuInfoData) {
+    //         // setTimeout(() => {
+    //         //     console.log(RestaurentMenuInfoData.data.cards[0].card.card.info.name);
+    //         // }, 10);
+            
+    //         console.log(RestaurentMenuInfoData.data.cards[0].card.card.info.name);
+    //     }
+    // }, [RestaurentMenuInfoData]);
+
+    // console.log(RestaurentMenuInfoData.data.cards[0].card.card.info.name);
+    
+    return (
+        <div>
         <h1>Restro Menue</h1>
-        <h1>Restro id : 123</h1>
-        <h1>Restrounnt 123</h1>
+        <h1>Restro id : {id}</h1>
+        {/* <h1>{RestaurentMenuInfoData.data.cards[0].card.card.info.name}</h1> */}
+        {RestaurentMenuInfoData && RestaurentMenuInfoData.data && RestaurentMenuInfoData.data.cards && RestaurentMenuInfoData.data.cards.length > 0 && (
+                <h1>{RestaurentMenuInfoData.data.cards[0].card.card.info.name}</h1>
+            )}
+        {/* <h1>{RestaurentMenuInfoData}</h1> */}
+        {/* <h1>Restro id : {param.id}</h1>  // not destructure of param
+        <h1>Restrounnt {param.id}</h1> */}
     </div>
   );
 };
