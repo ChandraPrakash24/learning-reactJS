0b3e6ffc4d1b8b7b7bded46124b08f135d3621fe
ChandraPrakash24 <2412prakas@gmail.com>
2024-03-04 14:16:21 +0530

    Learning CRM life cycle of react class based component (constructot, render, componentdidmount)

--- chapter-2/src/Components/Profile.js
+++ chapter-2/src/Components/Profile.js
@@ -3,12 +3,18 @@ import { useState } from "react";
 
 const Profile = (props) => {
   const [count, setCount] = useState(1);
+  const [countTwo, setCountTwo] = useState(2);
   return (
     <div> 
     <h3>This is a Profile Functional</h3>
     <h4>name: {props.name}</h4>
     <h4>xyz: {props.xyz}</h4>
     <h4>Count: {count}</h4>
+    <button onClick={()=>{
+        setCount(count+1)
+        setCountTwo(countTwo+1)
+      }}>Click</button>
+    <h4>Count: {countTwo}</h4>
     </div>
   )
 }
--- chapter-2/src/Components/ProfileClass.js
+++ chapter-2/src/Components/ProfileClass.js
@@ -8,11 +8,18 @@ class ProfileClass extends React.Component {
         super(props);
         this.state = {
             count: 0,
+            countTwo: 2,
         };
     };
 
+
+    componentDidMount() {
+        // best place for api call
+    }
+
     // render() { return (<>jsx</>) } it is only compolosury method other then from import
     render() {
+        // const {count} = this.state;
         return (
         <div>
             <h1>
@@ -21,6 +28,14 @@ class ProfileClass extends React.Component {
             <h4>Name: {this.props.name}</h4>
             <h4>xyz: {this.props.xyz}-class</h4>
             <h4>Count: {this.state.count}</h4>
+            {/* <h4>Count: {count}</h4> */}
+            <button onClick={()=>{
+                this.setState({
+                    count: this.state.count+1,
+                    countTwo: this.state.countTwo+1
+                })
+            }}>Cls-btn</button>
+            <h4>Count: {this.state.countTwo}</h4>
             {/* <h4>{this.props.match.params.name}</h4> */}
         </div>
         );
