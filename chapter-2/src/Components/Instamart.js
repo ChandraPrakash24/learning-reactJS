52ef5d28c252ab0d117c33383252e94a99bcd636
ChandraPrakash24 <2412prakas@gmail.com>
2024-03-14 11:00:28 +0530

    Creating Accordian for instamart

--- chapter-2/src/Components/Instamart.js
+++ chapter-2/src/Components/Instamart.js
@@ -1,7 +1,31 @@
+import { useState } from 'react';
+
+const Section = ({ title, discreption }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  return (
+    <div className="border border-black p-2 m-2 ">
+      <h2>{title}</h2>
+      {isVisible ? (
+        <button className="underline" type="button" onClick={() => setIsVisible(false)}>
+          Hide
+        </button>
+      ) : (
+        <button className="underline" type="button" onClick={() => setIsVisible(true)}>
+          Show
+        </button>
+      )}
+      {isVisible && <p>{discreption}</p>}
+    </div>
+  );
+};
+
 const Instamart = () => {
   return (
     <div>
-      <h1>Instamart</h1>
+      <h1 className="font-bold text-3xl m-2 p-2" >Instamart</h1>
+      <Section title={"About"} discreption={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, doloremque!"} />
+      <Section title={"Team"} discreption={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, doloremque!"} />
+      <Section title={"Career"} discreption={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, doloremque!"} />
     </div>
   );
 };
