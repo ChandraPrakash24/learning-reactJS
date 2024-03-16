835a19338bbb245f19790d7c44263752cf49a5a3
ChandraPrakash24 <2412prakas@gmail.com>
2024-03-16 11:41:38 +0530

    Optimizing Collepsable Accordian for instamart

--- chapter-2/src/Components/Instamart.js
+++ chapter-2/src/Components/Instamart.js
@@ -1,11 +1,11 @@
 import { useState } from 'react';
 
-const Section = ({ title, discreption, isVisible, setIsVisible }) => {
+const Section = ({ title, discreption, isVisible, setIsVisible, setHide }) => {
   return (
     <div className="border border-black p-2 m-2 ">
       <h2>{title}</h2>
       {isVisible ? (
-        <button className="underline" type="button" onClick={() => setIsVisible()}>
+        <button className="underline" type="button" onClick={() => setHide()}>
           Hide
         </button>
       ) : (
@@ -19,53 +19,16 @@ const Section = ({ title, discreption, isVisible, setIsVisible }) => {
 };
 
 const Instamart = () => {
-  const [sectionConfig, setSectionConfig] = useState({
-    showABout: false,
-    showTeam: false,
-    showCareer: false,
-    showProduct: false,
-    showDetails: false
-  });
+  const [sectionConfig, setSectionConfig] = useState("Team");
   
   return (
     <div>
       <h1 className="font-bold text-3xl m-2 p-2" >Instamart</h1>
-      <Section title={"About"} discreption={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, doloremque!"} isVisible={sectionConfig.showABout} setIsVisible={() => setSectionConfig({
-        showABout: true,
-        showTeam: false,
-        showCareer: false,
-        showProduct: false,
-        showDetails: false
-      })} />
-      <Section title={"Team"} discreption={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, doloremque!"} isVisible={sectionConfig.showTeam} setIsVisible={() => setSectionConfig({
-        showABout: false,
-        showTeam: true,
-        showCareer: false,
-        showProduct: false,
-        showDetails: false
-      })} />
-      <Section title={"Career"} discreption={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, doloremque!"} isVisible={sectionConfig.showCareer} setIsVisible={() => setSectionConfig({
-        showABout: false,
-        showTeam: false,
-        showCareer: true,
-        showProduct: false,
-        showDetails: false
-      })} />
-      <Section title={"Product"} discreption={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, doloremque!"} isVisible={sectionConfig.showProduct} setIsVisible={() => setSectionConfig({
-        showABout: false,
-        showTeam: false,
-        showCareer: false,
-        showProduct: true,
-        showDetails: false
-      })} />
-      <Section title={"Detail"} discreption={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, doloremque!"} isVisible={sectionConfig.showDetails} setIsVisible={() => setSectionConfig({
-        showABout: false,
-        showTeam: false,
-        showCareer: false,
-        showProduct: false,
-        showDetails: true
-        
-      })} />
+      <Section title={"About"} isVisible={sectionConfig === "About"} discreption={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, doloremque!"} setIsVisible={() => setSectionConfig("About")} setHide={() => setSectionConfig("None")} />
+      <Section title={"Team"} isVisible={sectionConfig === "Team"} discreption={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, doloremque!"} setIsVisible={() => setSectionConfig("Team")} setHide={() => setSectionConfig("None")}/>
+      <Section title={"Career"} isVisible={sectionConfig === "Career"} discreption={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, doloremque!"} setIsVisible={() => setSectionConfig("Career")} setHide={() => setSectionConfig("None")}/>
+      <Section title={"Product"} isVisible={sectionConfig === "Product"} discreption={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, doloremque!"} setIsVisible={() => setSectionConfig("Product")} setHide={() => setSectionConfig("None")}/>
+      <Section title={"Detail"} isVisible={sectionConfig === "Detail"} discreption={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, doloremque!"} setIsVisible={() => setSectionConfig("Detail")} setHide={() => setSectionConfig("None")}/>
     </div>
   );
 };
