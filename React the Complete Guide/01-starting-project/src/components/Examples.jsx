426671bb647b889d64b7b2f68f1e684741963039
ChandraPrakash24 <2412prakas@gmail.com>
2024-08-09 15:52:59 +0530

    moving up for new section

--- React the Complete Guide/01-starting-project/src/components/Examples.jsx
+++ React the Complete Guide/01-starting-project/src/components/Examples.jsx
@@ -1,7 +1,8 @@
-import { isValidElement, useState } from "react";
+import { useState } from "react";
 
 import { EXAMPLES } from "../utils/data.js";
 import TabButton from "./TabButton.jsx";
+import Tabs from "./Tabs.jsx";
 
 export default function Examples() {
   const [selectedTopic, setSelectedTopic] = useState(null);
@@ -12,49 +13,49 @@ export default function Examples() {
   }
 
   return (
-    <section id="examples">
-      <h2>Examples</h2>
-      <menu>
-        <TabButton
-          currActiveClass={selectedTopic === "components"}
-          onSelect={() => onSelect("components")}
-        >
-          Components
-        </TabButton>
-        <TabButton
-          currActiveClass={selectedTopic === "jsx"}
-          onSelect={() => onSelect("jsx")}
-        >
-          JSX
-        </TabButton>
-        <TabButton
-          currActiveClass={selectedTopic === "props"}
-          onSelect={() => onSelect("props")}
-        >
-          Props
-        </TabButton>
-        <TabButton
-          currActiveClass={selectedTopic === "state"}
-          onSelect={() => onSelect("state")}
-        >
-          State
-        </TabButton>
-      </menu>
-      {!selectedTopic && <p>Please select a topic.</p>}
-      {selectedTopic &&
-        <div id="tab-content">
-          <h3>
-            {EXAMPLES[selectedTopic].title}
-          </h3>
-          <p>
-            {EXAMPLES[selectedTopic].description}
-          </p>
-          <pre>
-            <code>
-              {EXAMPLES[selectedTopic].code}
-            </code>
-          </pre>
-        </div>}
+    <section title="Examples" id="examples">
+      <Tabs
+        ButtonsContainer="menu"
+        buttons={
+          <>
+            <TabButton
+              currActiveClass={selectedTopic === "components"}
+              onSelect={() => onSelect("components")}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              currActiveClass={selectedTopic === "jsx"}
+              onSelect={() => onSelect("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              currActiveClass={selectedTopic === "props"}
+              onSelect={() => onSelect("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              currActiveClass={selectedTopic === "state"}
+              onSelect={() => onSelect("state")}
+            >
+              State
+            </TabButton>
+          </>
+        }
+      >
+        {!selectedTopic && <p>Please select a topic.</p>}
+        {selectedTopic && (
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
+        )}
+      </Tabs>
     </section>
   );
 }
