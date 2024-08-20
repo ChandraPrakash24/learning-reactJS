96744212045a5d2161b761702999023ac8017f71
ChandraPrakash24 <2412prakas@gmail.com>
2024-08-20 17:41:10 +0530

    BUILDING WORDEL GAME

---
+++ project-wordle-game/src/components/GuessInput/GuessInput.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+function GuessInput() {
+  const [guessTerm, setGuessTerm] = useState("");
+
+  function handleGuessSubmit(e) {
+    e.preventDefault();
+
+    if (guessTerm.length < 5 || guessTerm.length > 5 || !guessTerm.trim())
+      return;
+
+    console.log(guessTerm);
+
+    setGuessTerm("");
+  }
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleGuessSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        required
+        minLength={5}
+        maxLength={5}
+        id="guess-input"
+        type="text"
+        value={guessTerm}
+        onChange={(e) => setGuessTerm(e.target.value.toUpperCase())}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
